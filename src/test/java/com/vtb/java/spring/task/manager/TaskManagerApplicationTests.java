49903eceeb3328a673712592323903417ef34ef2
vladder47 <vladisdrozdov@gmail.com>
2020-09-04 11:52:14 +0300

    First commit

---
+++ src/test/java/com/vtb/java/spring/task/manager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtb.java.spring.task.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
